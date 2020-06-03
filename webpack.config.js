de801e48d0b0db8788e1d5b7ca25242253e64ea6
Dias Kazhtai <dkazhtai21@mail.ru>
2020-06-03 12:14:01 +0500

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+
+
+module.exports = {
+    context:  path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    mode:'development',
+    output :{
+        filename:'bundle.[hash].js',
+        path:path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(),   
+        new CopyPlugin({
+            patterns: [
+              { 
+                  from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                   to: path.resolve(__dirname, 'dist')
+                 },
+            ],
+          }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+               MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader',
+            ],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ],
+      },
+}
