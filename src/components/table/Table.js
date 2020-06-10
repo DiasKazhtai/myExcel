7abf98d853e02676ff9f9ed30848a2d912ab1adf
Dias Kazhtai <dkazhtai21@mail.ru>
2020-06-10 17:58:37 +0300

    new table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -30,7 +30,7 @@ export class Table extends ExcelComponent {
     const $cell = this.$root.find('[data-id="0:0"]')
     this.selection.select($cell)
   }
-
+  arrow (){}
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event)
